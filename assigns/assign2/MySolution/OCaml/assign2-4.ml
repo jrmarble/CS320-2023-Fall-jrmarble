7246cf53360e18e1bf881350a228b98c058097ce
Reid Marble <reidmarble@Reids-Macbook.local>
2023-09-27 21:05:05 -0400

    bug fixes

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -18,7 +18,7 @@ string_sepjoin_list(";;")(["11";"22";"33"]) = "11;;22;;33"
 #use "./../../assign2.ml"
 #use "./../../../classlib/OCaml/MyOCaml.ml"
 
-let string_sepjoin_list (sep: string)(xs: string list): string =
+let string_sepjoin_list(sep: string)(xs: string list): string =
   match xs with
   | [] -> ""
   | x1::rest -> list_foldleft(rest)(x1)(fun acc s -> string_append(acc)(string_append(sep)(s)))
